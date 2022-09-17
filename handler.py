556d8eed83981881a9856e9bbcecdf4b6621ae92
zzjzzjz <2416971964@qq.com>
2022-09-17 21:01:51 +0800

    commit 2

--- handler.py
+++ handler.py
@@ -1,5 +1,6 @@
 import  socket
 import time
+UDP_RECV_DATA_SIZE=1024*100#udp一次接受数据的大小
 def getFiles(ip,port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((ip, port))
@@ -32,16 +33,36 @@ def getFileByTCP(ip,port):
     client.close()
 
 def getFileByUDP(ip,port):
+    id=0#请求文件数据偏移量
     fileName = input("下载文件名：")
-    file = open(fileName, "wb")
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.bind(('127.0.0.1',5678))
-    data={'type':1,'fileName':fileName}#type=1代表发送文件请求
-    client.sendto(str(data).encode('utf-8'),(ip,port))
-
-    data,addr=client.recvfrom(1024*10)
+    sendData = {'fileName': fileName, 'id': id}#构造请求数据，包括文件名，
+    client.sendto(str(sendData).encode('utf-8'),(ip,port))
+    data,addr=client.recvfrom(UDP_RECV_DATA_SIZE)
     print(data)
     data=eval(data.decode('utf-8'))
+    if not data['ok']:#响应信息表示错误则退出该函数
+        print('文件请求错误或服务器出问题')
+        return
+    file = open(fileName, "wb")
+    while not data['end']:
+        print(id)
+        if data['id']==id:#响应数据的是想要的数据
+            file.write(data['fileData'])
+            id=id+1
+        sendData = {'fileName': fileName, 'id': id}
+        client.sendto(str(sendData).encode('utf-8'), (ip, port))
+        data, addr = client.recvfrom(UDP_RECV_DATA_SIZE)
+        data=eval(data.decode('utf-8'))
+    file.close()
+
+
+
+
+
+
+    """
     while True:
         if data['end']:
             file.close()
@@ -62,11 +83,8 @@ def getFileByUDP(ip,port):
             data = eval(data.decode('utf-8'))
 
 
-
-
-
-
     print('结束')
+    """
 
 
 
