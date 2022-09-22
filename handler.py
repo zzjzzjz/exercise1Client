8616942ec61f3f5060cc37aea00991c4518bc665
kaneki_my <2795188612@qq.com>
2022-09-22 10:28:11 +0800

    代码合并，UDP传输小bug

--- handler.py
+++ handler.py
@@ -2,10 +2,13 @@ import hashlib
 import os
 import socket
 import time
+from math import ceil
+
 import select
 import re
 import sys
-UDP_RECV_DATA_SIZE = 1024 * 100  # udp一次接受数据的大小
+
+UDP_RECV_DATA_SIZE = 1024 * 10000  # udp一次接受数据的大小
 
 
 def getFiles(ip, port):
@@ -15,7 +18,6 @@ def getFiles(ip, port):
     print("---------------可下载文件列表---------------")
     try:
         while True:
-
             ready = client.recv(1024).decode('utf8')
             length = int(ready)
             client.send(ready.encode('utf8'))
@@ -36,8 +38,9 @@ def getFileByTCP(ip, port):
     client.connect((ip, port))
     client.send(fileName.encode('utf-8'))
     data = b''
-    data, addr = client.recvfrom(1024)
-    print(data)
+    data, addr = client.recvfrom(35) # 在这一行打断点会出问题，不够稳定，打断点暂停的时间缓存已经有更多的数据，此时data会掺杂其他的东西
+    # bug 形如 ---> print(data) b'{\'ok\': True, \'fileSize\': 155921579}\x00\x00\x00\x14ftypiso\n'
+    print(data)# b"{'ok': True, 'fileSize': 155921579}"
     data = eval(data.decode('utf-8'))
 
     if not data['ok']:
@@ -45,16 +48,18 @@ def getFileByTCP(ip, port):
         client.close()
         return
     i = 1
-    fileName=re.split('[/\\\]',fileName)[-1]
-    file = open("folder\\"+fileName, "wb")#下载文件保存在floder目录下
-    fileSize=data['fileSize']
+    fileName = re.split('[/\\\]', fileName)[-1]
+    filepath = "folder\\" + fileName
+    file = open(filepath, "wb")  # 下载文件保存在floder目录下
+    fileSize = data['fileSize']
+    timebefore = time.time()
     while True:
-        data = client.recv(1024*10)
+        data = client.recv(1024 * 10)
         i = i + 1
         per = int((os.stat("folder\\" + fileName).st_size / fileSize) * 100)
-        print("进度: {}%: ".format(per),"" , end="")
-        sys.stdout.flush()
-
+        # print("进度: {}%: ".format(per),"▋" * (per // 2) , end="")
+        # sys.stdout.flush()
+        # time.sleep(0.05)
 
         if not data:
             break
@@ -63,7 +68,8 @@ def getFileByTCP(ip, port):
     file.close()
     print("\r", end="")
     print("进度: {}%:\n ".format(int((os.stat("folder\\" + fileName).st_size / fileSize) * 100)), "", end="")
-
+    timeafter = time.time()
+    print('大小' + str(os.path.getsize(filepath)/pow(2,20))+ 'MB' + '总用时：' + str(timeafter - timebefore) + ' s')
     client.close()
 
 
@@ -72,16 +78,14 @@ def getFileByUDP(ip, port):
     fileName = input("下载文件名：")
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.bind(('127.0.0.1', 5678))
-
-
-
-
-    id=0
-    file = open("folder\\" + re.split('[/\\\]', fileName)[-1], "wb")  # 下载文件保存在floder目录下
-    i=0#失败次数，超过5次判定网络有问题
+    filepath = "folder\\" + re.split('[/\\\]', fileName)[-1]
+    id = 0
+    file = open(filepath, "wb")  # 下载文件保存在floder目录下
+    i = 0  # 失败次数，超过5次判定网络有问题
+    timebefore = time.time()
     while True:
 
-        if i>5:
+        if i > 5:
             print('请检测网络')
             file.close()
             return
@@ -93,12 +97,12 @@ def getFileByUDP(ip, port):
             data, addr = client.recvfrom(UDP_RECV_DATA_SIZE)
             data = eval(data.decode('utf-8'))
         except:
-            i=i+1
+            i = i + 1
             continue
         if not __testMd5OfDict(data):
-            i=i+1
+            i = i + 1
             continue
-        if data['ok']==False:  # 响应信息表示错误则退出该函数
+        if data['ok'] == False:  # 响应信息表示错误则退出该函数
             print('\n文件不存在或服务器出问题')
             file.close()
             return
@@ -106,17 +110,16 @@ def getFileByUDP(ip, port):
             file.write(data['fileData'])
             id = id + 1
 
-        print("\r", end="")
-        print("进度: {}%: ".format(data['per']),"" , end="")
+        # print("\r", end="")
+        # print("进度: {}%: ".format(data['per']),"" , end="")
         if data['end']:
             break
-        i=0
-
-
-
+        i = 0
 
     print("\r", end="")
     print("进度: {}%: \n".format(data['per']), "", end="")
+    timeafter = time.time()
+    print('大小' + str(os.path.getsize(filepath) / pow(2,20)) + 'MB' + '总用时：' + str(timeafter - timebefore) + ' s')
     file.close()
 
 
@@ -132,29 +135,3 @@ def __testMd5OfDict(ob: dict):  # 测试字典的md5值
         return True
     else:
         return False
-
-    """
-    while True:
-        if data['end']:
-            file.close()
-            break
-        id=data['id']
-        print(id)
-        file.seek(id*1024)
-        file.write(data['data'])
-        data={'type':2,'id':id+1}#下一个请求序号
-        client.sendto(str(data).encode('utf-8'),addr)
-
-        data, addr = client.recvfrom(1024*10)
-        print(data)
-        data = eval(data.decode('utf-8'))
-        while data['id']!=id+1:
-            client.sendto(str(data).encode('utf-8'),addr)
-            data, addr = client.recvfrom(1024 * 10)
-            data = eval(data.decode('utf-8'))
-
-
-    print('结束')
-    """
-
-
--- main.py
+++ main.py
@@ -6,9 +6,8 @@ PORT1 = 9090  # 对应三种服务的端口
 PORT2 = 9091
 PORT3 = 9092
 if __name__ == "__main__":
-    print('1----查看文件列表\n2----tcp下载文件\n3----udp下载文件\n4----退出程序')
+    print('1----查看文件列表\n2----tcp下载文件\n3----udp下载文件\n4----退出')
     selected = input()
-
     while True:
         if selected == '1':
             handler.getFiles(IP, PORT1)
@@ -17,10 +16,9 @@ if __name__ == "__main__":
         elif selected == '3':
             handler.getFileByUDP(IP, PORT3)
         elif selected == '4':
-            print('退出程序')
-            exit()
+            exit(0)
         else:
             print('输入错误')
         print('-------------------------------')
-        print('1----查看文件列表\n2----tcp下载文件\n3----udp下载文件')
+        print('1----查看文件列表\n2----tcp下载文件\n3----udp下载文件\n4----退出')
         selected = input()
